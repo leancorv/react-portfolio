bb3632f4381007d983b07f1ba4cad75b42c9da50
Leandro <lecorvalan2@gmail.com>
2022-06-29 02:25:58 -0300

    Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import CTA from './CTA'
 import './header.css'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Leandro Corvalán</h1>
+        <h5 className="text-light">Fullstack developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll down</a>
+      </div>
+    </header>
   )
 }
 
