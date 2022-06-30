9ccb14797c217ed553b2965d0853aeee05b46ab8
Leandro <lecorvalan2@gmail.com>
2022-06-30 02:43:17 -0300

    Slider with SwiperJS (for testimonials)

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,15 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
   {
     avatar: AVTR1,
@@ -34,11 +43,16 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
         {
           data.map(({avatar, name, review}) => {
             return (
-              <article key={name} className="testimonial">
+              <SwiperSlide key={name} className="testimonial">
                 <div className="client__avatar">
                   <img src={avatar} alt={name}/>
                 </div>
@@ -46,11 +60,11 @@ const Testimonials = () => {
                 <small className="client__review">
                   {review}
                 </small>
-              </article>
+              </SwiperSlide>
             )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
