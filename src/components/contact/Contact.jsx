b1c0a82845ce2e8d319b6fa11bd2329dd014fd05
Leandro <lecorvalan2@gmail.com>
2022-06-30 17:11:15 -0300

    Email form with EmailJS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {BsTelegram} from 'react-icons/bs'
 import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wbjt5vz', 'template_7t6h7p9', form.current, 'Cz6_lmzCMcTE7eIVX')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,7 +48,7 @@ const Contact = () => {
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
