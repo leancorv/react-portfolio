922195369612cbda92e02bab23f981d0184485e1
Leandro <lecorvalan2@gmail.com>
2022-07-01 02:18:42 -0300

    Footer Component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {FaGithub} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="/#" className='footer__logo'>Leancorv</a>
+      <ul className='permalinks'>
+        <li><a href="/#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://twitter.com"><BsLinkedin /></a>
+        <a href="https://github.com/lecov/"><FaGithub /></a>
+        <a href="https://instagram.com"><FiInstagram /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; LeanDev. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
